83cc6a70d5010cfeaedff3c027fca9bdabff400b
saracarretero <carreteroprogramar@gmail.com>
2025-10-09 21:09:29 -0300

    incorporación de readme

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterApp } from './router/RouterApp.jsx'
-import './index.css'
+import './styles/index.css'
 import { ChatProvider } from './context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
--- src/views/Help.jsx
+++ src/views/Help.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom"
+import '../styles/help.css'
 
-const Help = () => {
+export default function Help() {
   return (
     <section>
-      <h1>Página Help</h1>
+      <h2>Página Help</h2>
       <Link to="/chat">Volver al chat</Link>
     </section>
   )
 }
-
-export { Help }
